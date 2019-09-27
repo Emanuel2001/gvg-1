c454f44459ad3e9b4afb5e7261cf2a8681fd043b
Emanuel2001 <emanuel.zakanj@gmail.com>
2019-09-27 13:20:16 +0200

    Update 2_zadatak.py

--- vjezbe/2_zadatak.py
+++ vjezbe/2_zadatak.py
@@ -1,2 +1,10 @@
 #ZADATCI(riješite, dokumentirajte I zatim pročitajte docstring): 
 #Isprogramirajte funkciju fib(n) koja računa n-ti član Fibonaccijevog niza (niz kod kojeg je svaki član definiran kao zbroj prethodna dva, a javlja se pri analizi idealizirane populacije zečeva). 
+
+def fib(n):
+    if n<=1:
+        return n
+    else:
+        return(fib(n-1) + fib(n-2))
+x=int(input())
+print(fib(x))
